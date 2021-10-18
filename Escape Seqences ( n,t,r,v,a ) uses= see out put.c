de89328edbbc53abc87e03eeead603094a9c4260
RavitejaPolavarapu <92290708+RavitejaPolavarapu@users.noreply.github.com>
2021-10-18 12:16:59 +0530

    Add files via upload

---
+++ Escape Seqences ( n,t,r,v,a ) uses= see out put.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf("\\n = is used to skip the line \n");
+	printf("\\t = Horizontal tab space --> give tab space b/w two words\n");
+	printf("\\r = caariage return --> To bring the cursor to the beginning and\n   it replaces the starting letters with the letters which are entered after the \\r\n");
+	printf("\\v = vertical tab space\n");
+	printf("\\a = audible bell ---> it will make the computer to make one small sound\n");
+	printf("\\ = we can use any special character after \\to get that character in output");
+}
