f797d450da7d0bc584622a1c8ef52cb7cdaf60a4
RavitejaPolavarapu <92290708+RavitejaPolavarapu@users.noreply.github.com>
2021-10-20 12:20:59 +0530

    Add files via upload

---
+++ divison operation on float data type.c
@@ -0,0 +1,14 @@
+//division of float
+
+#include<stdio.h>
+void main()
+{
+	//variable declaration and intialization
+	
+	float a=45.4,b=75.95,c=a/b; 
+	
+	//output
+	
+	printf("Division = %f",c);
+	
+}
