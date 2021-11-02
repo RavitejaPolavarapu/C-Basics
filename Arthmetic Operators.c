e46c840c463b619c242fb181de99da96914d9132
RavitejaPolavarapu <92290708+RavitejaPolavarapu@users.noreply.github.com>
2021-11-02 11:35:22 +0530

    Add files via upload

---
+++ Arthmetic Operators.c
@@ -0,0 +1,13 @@
+//Arthematic Operators
+#include<stdio.h>
+void main()
+{
+	int a=30,b=12;
+	
+	int c=a+b,d=a-b,e=a/b,f=a%b; // % -->To Find the Reminder
+	
+	printf("Sum of %d and %d: %d\n",a,b,c);
+	printf("difference of %d and %d: %d\n",a,b,d);
+	printf("Qoutent of %d and %d: %d\n",a,b,e);
+	printf("Reminder of %d and %d: %d",a,b,f);
+}
