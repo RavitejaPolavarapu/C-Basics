bc114cbc35691a83e35f602788b9df9b0f72ad0c
RavitejaPolavarapu <92290708+RavitejaPolavarapu@users.noreply.github.com>
2021-11-02 12:23:56 +0530

    Add files via upload

---
+++ sum of n natural numbers.c
@@ -0,0 +1,12 @@
+// Sum of n natural Numbers
+
+#include<stdio.h>
+void main()
+{
+	//formula n*(n+1)/2
+
+	int n=100,sum=n*(n+1)/2;
+	
+	printf("Sum of First %d Natural Numbers is %d",n,sum);
+	
+}
