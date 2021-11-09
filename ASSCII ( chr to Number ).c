8c6046c2b45006ab940dc5902254579bd9dce649
RavitejaPolavarapu <92290708+RavitejaPolavarapu@users.noreply.github.com>
2021-11-09 12:29:36 +0530

    Add files via upload

---
+++ ASSCII ( chr to Number ).c
@@ -0,0 +1,9 @@
+//ASCII ( chr to number )
+#include<stdio.h>
+void main()
+{
+	char c;
+	printf("enter a character(a-z or A-Z):");
+	scanf("%c",&c);
+	printf("Corresponding ASCII number for %c is %d",c,c);
+}
